f3d4d04ed56369600500aa1430df61010737dee7
Dbirtic <Dominik.birtic666@gmail.com>
2021-05-27 19:57:00 +0200

    removed the button because it was for debug purposes

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".press").addEventListener('click', () =>{
-    counter++;
-    console.log(counter);
-    document.getElementById("counter").innerHTML = counter;
-});
-
-
