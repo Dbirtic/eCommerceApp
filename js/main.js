16cca261da806474b8b12393bae331350f3dd0a5
Dbirtic <Dominik.birtic666@gmail.com>
2021-05-31 20:50:38 +0200

    added some code for displaying product data onto page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+const productList = document.getElementById("product-list");
+
+// function which will display products onto page with their info
+// work in progress though
 const searchData = async () =>{
-    const res = await fetch('../data/data.js');
+    const res = await fetch('../data/data.json');
     const data = await res.json();
 
-    console.log(data);
+    console.log(typeof(data));
+    console.log(data.length);
+
+    if(data.length > 0){
+        const html = data.map(info => `
+        <div class="card card-body mb-1">
+            <h4>${info.brand}</h4>
+        </div>
+        `).join('');
+
+        productList.innerHTML = html;
+    }
 }
 
 searchData();
