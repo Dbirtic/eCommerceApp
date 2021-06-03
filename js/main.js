be08abe119b0f7658402107274daa302f7a8e4a3
Dbirtic <Dominik.birtic666@gmail.com>
2021-06-03 17:27:42 +0200

    continuing work on css for products

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ const searchData = async () =>{
 
     if(data.length > 0){
         const html = data.map(info => `
-        <div class=".products">
+        <div class="products">
             <h4>${info.name}</h4>
             <h4>${info.category}</h4>
             <img src="${info.image}" width="100" height="200">
-            <h4>${info.price}</h4>
+            <h4>${info.price}$</h4>
             <h4>${info.brand}</h4>
         </div>
         `).join('');
