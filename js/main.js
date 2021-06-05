b03e8bfed74c868c23be69b9ac90d78028ddc3ed
Dbirtic <Dominik.birtic666@gmail.com>
2021-06-05 11:11:00 +0200

    started work on adding every product in local storage and added link to products. Need to finish the local storage storaging and maybe dynamic creation of product pages with templates

--- js/main.js
+++ js/main.js
@@ -9,20 +9,21 @@ const searchData = async () =>{
     if(data.length > 0){
         data.map(info =>{
             let product = localStorage.getItem('product');
+            console.log(info);
             if(product === null)
             {
                 // Next to do!
                 // napraviti spremanje svega iz data.json-a na local storage
-                product += info;
+                localStorage.setItem('product', JSON.stringify({product}));
             }
             console.log(product);
         });
-        const html = data.map(info => `
+        const html = data.map(info =>`
         <div class="products">
             <ul>
                 <li>Name: ${info.name}</li>
                 <li>Category: ${info.category}</li>
-                <li><img src="${info.image}" width="100" height="200"></li>
+                <li><a href="/products/${info._id}"><img src="${info.image}" width="100" height="200"></a></li>
                 <li>Price: ${info.price}$</li>
                 <li>${info.brand}</li>
             </ul>
