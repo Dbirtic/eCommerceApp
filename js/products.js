4eae5b335244f4250b20918de28aaee1413b88db
Dbirtic <Dominik.birtic666@gmail.com>
2021-06-15 03:52:22 +0200

    added products.js which will be used for displaying products on product pages and also added that js file to the product page 1

---
+++ js/products.js
@@ -0,0 +1,8 @@
+// TO DO:
+// write code for paring the id from the url and if it matches with the id from the data then display that product
+
+
+const products = document.querySelector('.product-data');
+
+products.innerHTML = `<h1>Subject to Change</h1>`;
+
