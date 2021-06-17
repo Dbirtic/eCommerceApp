d38c89acaac997b61fd7379b4c8355dea76d8341
Dbirtic <Dominik.birtic666@gmail.com>
2021-06-17 19:42:13 +0200

    turned localStorage data to object

--- js/products.js
+++ js/products.js
@@ -9,8 +9,10 @@ console.log(productNum);
 // TO DO: use map function and compare the data with the product number to display it's data
 // data can be also taken from the localStorage
 
-const data = localStorage.getItem('product');
+const data = JSON.parse(localStorage.getItem('product'));
 
 console.log("data: "+data);
+
+console.log("data[0]: " + data[0]);
 
 products.innerHTML = `<h1>Subject to Change</h1>`;
