20b3c8df53505b2619bfee90950429d8747dfe19
Dbirtic <Dominik.birtic666@gmail.com>
2021-06-25 21:52:42 +0200

    removed some comments from product.js, made main container larger, changed css for buttons

--- js/products.js
+++ js/products.js
@@ -6,20 +6,15 @@ const url = document.location.pathname.split('/');
 const productNumArr = url[2].split('.');
 const productNum = productNumArr[0];
 
-console.log(productNum);
 // TO DO: use map function and compare the data with the product number to display it's data
 // data can be also taken from the localStorage
 
 const data = JSON.parse(localStorage.getItem('product'));
 
-//console.log("data: "+data);
-
-//console.log("data[0]: " + data[0]);
-console.log(typeof data);
-console.log(data['data']);
+/*console.log(typeof data);
+console.log(data['data']); */
 
 data['data'].map(product => {
-    //console.log(product);
     if(product._id === productNum){
         console.log(product.image);
         products.innerHTML = `
