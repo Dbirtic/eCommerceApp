76d42feb81e0aade90f7a62533774c0e5cc9d294
Dbirtic <Dominik.birtic666@gmail.com>
2021-07-09 22:58:32 +0200

    added class cart-list to the cart page, and started working on the displaying the items added to cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,10 @@
+const cartList = document.querySelector('.cart-list');
+
 // add function to display cart
 function showCart (){
-    
+    const products = JSON.parse(localStorage.getItem('cart'));
+    console.log(products);
+    //cartList.innerHTML += ``
 }
 
 showCart();
