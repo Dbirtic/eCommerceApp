afae59381f9192f88aea29b5b46a3c642dd1225e
Dbirtic <Dominik.birtic666@gmail.com>
2021-07-09 20:28:55 +0200

    started working on cart page to display cart items

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,4 @@
-const cart = `
-<div class="grid-container">
-    <div class="header">
-        <div class="navi">
-            <a href="/index.html">Home</a>
-            <a href="/profile.html">Profile</a>
-            <a href="/cart.html" class="active">Cart</a>
-            <a href="/#">Log In</a>
-            <a href="/#">Register</a>
-        </div>
-        <h1>Cart</h1>
-        <h1 id="counter"></h1>
-    </div>
-    <div class="main-container">
-        <h1>Welcome to my Cart</h1>
-</div>
-<footer>
-    <h1>2021@dbirtic</h1>
-</footer>
-`;
+// add function to display cart
+function showCart (){
+    
+}
