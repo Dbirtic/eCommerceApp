ce13896eac9aeefa7eccc527fe0949b37d8d6f62
Dbirtic <Dominik.birtic666@gmail.com>
2021-07-14 19:03:09 +0200

    fixed the display of items in cart

--- js/cart.js
+++ js/cart.js
@@ -3,16 +3,17 @@ const cartList = document.querySelector('.cart-list');
 // add function to display cart
 function showCart (){
     const products = JSON.parse(localStorage.getItem('cart'));
-    console.log(products);
-    console.log(products[0]);
-    cartList.innerHTML += `
-    <ul>
-        <li>Product name: ${products.name}</li>
-        <li><img src="${products.image}" width="75" height="125"></li>
-        <li>${products.price}$</li>
-        <li>Number of items in stock: ${products.countInStock}</li>
-    </ul>
-    `;
+    products.map(item =>{
+        cartList.innerHTML += `
+        <ul>
+            <li>Product name: ${item.name}</li>
+            <li><img src="${item.image}" width="75" height="125"></li>
+            <li>${item.price}$</li>
+            <li>Number of items in stock: ${item.countInStock}</li>
+        </ul>
+        `;
+    });
+    
 }
 
 showCart();
